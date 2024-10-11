75945d8652a4eab3433615ea7abec95af1e067a1
basel-abdelazim <basel.abdelazim@gmail.com>
2024-10-11 08:35:03 +0200

    Update productRouter.js

---
+++ routers/productRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { getProducts, createProduct, getProductById, updateProduct, deleteProduct } = require('../controllers/products');
+const router = express.Router();
+
+router.get('/', getProducts);
+router.post('/', createProduct);
+router.get('/:id', getProductById);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
+
+module.exports = router;
