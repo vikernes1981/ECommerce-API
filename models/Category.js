a96de22d2f0d0236eb3dd37238e5d28a350e8552
basel-abdelazim <basel.abdelazim@gmail.com>
2024-10-11 08:32:39 +0200

    Update Category.js

---
+++ models/Category.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const Category = sequelize.define('Category', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+module.exports = Category;
