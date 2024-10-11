7a0d781c79720eb5e13a8c8c7f1f0a1229b24f61
basel-abdelazim <basel.abdelazim@gmail.com>
2024-10-11 08:31:23 +0200

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,1 +1,48 @@
+const User = require('../models/User');
+const { userSchema, userUpdateSchema } = require('../schemas/userSchemas');
+
+// Get all users
+const getUsers = async (req, res) => {
+    const users = await User.findAll();
+    res.json(users);
+};
+
+// Create a user
+const createUser = async (req, res) => {
+    const { error } = userSchema.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+};
+
+// Get a specific user by ID
+const getUserById = async (req, res) => {
+    const user = await User.findByPk(req.params.id);
+    if (!user) return res.status(404).json({ error: 'User not found' });
+    res.json(user);
+};
+
+// Update a user by ID
+const updateUser = async (req, res) => {
+    const { error } = userUpdateSchema.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    const user = await User.findByPk(req.params.id);
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    await user.update(req.body);
+    res.json(user);
+};
+
+// Delete a user by ID
+const deleteUser = async (req, res) => {
+    const user = await User.findByPk(req.params.id);
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    await user.destroy();
+    res.status(204).send();
+};
+
+module.exports = { getUsers, createUser, getUserById, updateUser, deleteUser };
 
